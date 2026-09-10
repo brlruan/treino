c0810968f03206dd9b1deac3ff13dc4ed1ec743d
brlruan <sigilobh24@gmail.com>
2026-09-10 09:49:35 -0300

    Refresh offline cache for history transfer update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fullbody-v3';
+const CACHE = 'fullbody-v4';
 const FILES = [
   '/treino/treino.html',
   '/treino/manifest.json'
@@ -14,7 +14,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k.startsWith('fullbody-') && k !== CACHE).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
